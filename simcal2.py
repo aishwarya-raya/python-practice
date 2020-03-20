751f7ab00f53f5c588f8af60754eeae4baa6d258
aishwarya-raya <56466591+aishwarya-raya@users.noreply.github.com>
2020-03-20 20:54:27 +0530

    New version

--- simcal2.py
+++ simcal2.py
@@ -1,11 +1,26 @@
 # Write your code here :-)
 # a program to enter expressions into a simple calculator
-no1,op,no2=input('Enter your expression: ').split()
-if op=='+':
-    print(no1+op+no2+'='+str(int(no1)+int(no2)))
-elif op=='-':
-    print(no1+op+no2+'='+str(int(no1)-int(no2)))
-elif op=='*':
-    print(no1+op+no2+'='+str(int(no1)*int(no2)))
-elif op=='/':
-    print(no1+op+no2+'='+str(int(no1)/int(no2)))
+ch='y'
+while ch=='y':
+    n=0
+    c='y'
+    no1,op,no2=input('Enter your expression: ').split()
+    while c=='y':
+        if n>0:
+            print(ans)
+            op,no2=input().split()
+        if op=='+':
+            ans=int(no1)+int(no2)
+        elif op=='-':
+            ans=int(no1)-int(no2)
+        elif op=='*':
+            ans=int(no1)*int(no2)
+        elif op=='/':
+            ans=int(no1)/int(no2)
+        print(str(no1)+op+no2,'=',ans)
+        no1=ans
+        n=n+1
+        print('Would you like to continue here?')
+        c=input()
+    print('Would you like to start another operation?')
+    ch=input()
