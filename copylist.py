db40c53defcfb3c314a9e4557aab9ca4893ccaa3
aishwarya-raya <56466591+aishwarya-raya@users.noreply.github.com>
2020-03-25 19:06:28 +0530

    A python program to clone or copy a list

---
+++ copylist.py
@@ -0,0 +1,8 @@
+# Write your code here :-)
+# a python program to clone or copy a list
+print('Enter the values into the list: ')
+list1=map(int,input().split())
+list2=[]
+for num in list1:
+    list2.append(num)
+print(list2)
